2c29df5548fff706c0488bd3ea23e94431712171
Marco Antonio Estelles <tonovarela@tonovarelamac.local>
2018-06-05 00:01:03 -0500

    Variable Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/cafe';
 }else{
-    urlDB="mongodb://cafe-user:Varela123@ds247830.mlab.com:47830/cafe";
+    urlDB=process.env.MONGO_URI;
 }
 
 process.env.urlDB=urlDB;
